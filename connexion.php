c80e39f1ecd7913e47620a1a3b49a90b7484267c
MathissGit <mathissineux@gmail.com>
2024-01-09 14:40:32 +0100

    includes

--- connexion.php
+++ connexion.php
@@ -35,15 +35,7 @@
   }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Page de Connexion</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
-<body class="bg-dark">
+<?php require_once('includes/header.php'); ?>
   <div class="container position-absolute top-50 start-50 translate-middle">
     <div class="row justify-content-center mt-5">
       <div class="col-md-6">
@@ -71,6 +63,4 @@
     </div>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php require_once('includes/footer.php'); ?>
