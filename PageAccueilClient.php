bfca87a18c07b846e8f2e65bc8e277a532a027cf
MaximePaquotte <maximepaquotte9@gmail.com>
2024-01-12 15:19:02 +0100

    maj

--- PageAccueilClient.php
+++ PageAccueilClient.php
@@ -48,7 +48,7 @@ require('includes/header.php');
       <div class="row">
         <div class="col-sm-4 offset-md-1 py-4">
           <ul class="list-unstyled">
-            <li><a href=" " class="text-white">Circuit disponible : <?php echo $circuit ?></a></li>
+            <li><a href="PageCircuit.php" class="text-white">Circuit disponible : <?php echo $circuit ?></a></li>
             <li><a href="CircuitsReserves.php" class="text-white">Mes reservations : <?php echo $reservationClient ?></a></li>
             <li><a href="connexion.php" class="text-white">Se déconnecter</a></li>
           </ul>
@@ -154,7 +154,7 @@ require('includes/header.php');
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Circuit 1" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Circuit 1</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Circuit 1</text></svg>
             <div class="card-body">
-              <p class="card-text">Circuit 1 de "" à ""</p>
+              <p class="card-text">Circuit 1 de Créteil à Lovran</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -169,7 +169,7 @@ require('includes/header.php');
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Circuit 2</text></svg>
             <div class="card-body">
-              <p class="card-text">Circuit 2 de "" à ""</p>
+              <p class="card-text">Circuit 2 de Khuean à Postira</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -184,7 +184,7 @@ require('includes/header.php');
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Circuit 3</text></svg>
             <div class="card-body">
-              <p class="card-text">Circuit 3 de "" à ""</p>
+              <p class="card-text">Circuit 3 de Zhishan à Long Hồ</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -200,12 +200,12 @@ require('includes/header.php');
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Circuit 4</text></svg>
             <div class="card-body">
-              <p class="card-text">Circuit 4 de "" à ""</p>
+              <p class="card-text">Circuit 4 de Majayjay à Bourges</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Voir</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Modifier</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Supprimer</button>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Réserver 
+                </button>
                 </div>
               </div>
             </div>
@@ -215,12 +215,12 @@ require('includes/header.php');
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Circuit 5</text></svg>
             <div class="card-body">
-              <p class="card-text">Circuit 5 de "" à ""</p>
+              <p class="card-text">Circuit 5 de Estarreja à Yuza</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Voir</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Modifier</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Supprimer</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Réserver 
+                </button>
                 </div>
               </div>
             </div>
@@ -230,12 +230,12 @@ require('includes/header.php');
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Circuit 6</text></svg>
             <div class="card-body">
-              <p class="card-text">Circuit 6 de "" à ""</p>
+              <p class="card-text">Circuit 6 de Tamamura à Yueyahe</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Voir</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Modifier</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Supprimer</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Réserver 
+                </button>
                 </div>
               </div>
             </div>
--- PageCircuit.php
+++ PageCircuit.php
@@ -1,4 +1,4 @@
-<link href="style.css" rel="stylesheet">
+
 
 <?php 
 
@@ -10,10 +10,36 @@ if (session_status() == PHP_SESSION_NONE) {
 
 require('server_db.php');
 require_once('includes/header.php');
+?>
 
+<header data-bs-theme="dark">
+  <div class="collapse text-bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4 offset-md-1 py-4">
+          <ul class="list-unstyled">
+            <li><a href="PageCircuit.php" class="text-white">Circuit disponible : <?php echo $circuit ?></a></li>
+            <li><a href="CircuitsReserves.php" class="text-white">Mes reservations : <?php echo $reservationClient ?></a></li>
+            <li><a href="connexion.php" class="text-white">Se déconnecter</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container">
+      <a href="PageAccueilClient.php" class="navbar-brand d-flex align-items-center">
+      <img src= img/logoBDD.png width="40" height="40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle>
+        <strong>EPSI Travel</strong>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
 
-
-
+<?php
 // exécuter la requête SQL pour récupérer les données de la table circuit
 $sql = "SELECT * FROM circuit";
 $result = mysqli_query($connexion, $sql);
@@ -54,7 +80,6 @@ while ($row = mysqli_fetch_assoc($result)) {
   echo "<form action='Circuit$id.php'>
           <button type='submit' class='btn btn-sm btn-outline-secondary'>Voir</button>
         </form>";
-  echo "<button>Réserver</button>";
   echo "</li>";
 }
 echo "</ul>";
