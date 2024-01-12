463ec5b246d22dcc0c382c0b7268801631ffe4aa
MaximePaquotte <maximepaquotte9@gmail.com>
2024-01-12 14:19:12 +0100

    maj

--- editCircuit.php
+++ editCircuit.php
@@ -1,6 +1,14 @@
 <?php
 
-require('server_db.php');
+
+  // se connecter à la base de données
+  $con = mysqli_connect("localhost", "root", "", "IHM_Gaber");
+  // vérifier la connexion
+  if (mysqli_connect_errno()) {
+    echo "Échec de la connexion : " . mysqli_connect_error();
+    exit();
+  }
+
 require_once('includes/header.php');
 
 $description = $_POST["description"]; 
@@ -15,7 +23,7 @@ $prix = $_POST["prix"];
 
 // Préparation de la requête SQL
 $sql = "INSERT INTO Circuit (Descri, Ville_dep, Pays_dep, Ville_arr, Pays_arr, Date_dep, Nb_PlaceDisp, Duree_Circ, Prix_insc) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"; // Requête SQL avec des paramètres
-$stmt = $connection->prepare($sql); // Préparation de la requête
+$stmt = $con->prepare($sql); // Préparation de la requête
 $stmt->bind_param("ssssssiii", $description, $ville_depart, $pays_depart, $ville_arrivee, $pays_arrivee, $date_depart, $nb_place, $duree, $prix); // Liaison des paramètres avec les valeurs
 
 // Exécution de la requête SQL
@@ -27,6 +35,6 @@ if ($stmt->execute()) {
 
 // Fermeture de la connexion
 $stmt->close(); 
-$connection->close(); 
+$con->close(); 
 
 ?>
